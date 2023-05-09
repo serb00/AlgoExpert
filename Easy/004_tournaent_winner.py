f8c1bc68a650d9cf28fa632b77fe50129fdaf7c1
Sergei Bozhko <sergei.bozhko@proton.me>
2023-05-09 14:03:23 +0200

    004 solved

--- Easy/004_tournaent_winner.py
+++ Easy/004_tournaent_winner.py
@@ -1,13 +1,31 @@
 def tournamentWinner(competitions, results):
     # Write your code here.
-    return ""
+    i = 0
+    res = dict()
+    while i < len(results):
+        home_team, away_ream = competitions[i]
+        result = results[i]
+        if result == 1:
+            if res.get(home_team) is not None:
+                res[home_team] = res[home_team] + 3
+            else:
+                res[home_team] = 3
+        else:
+            if res.get(away_ream) is not None:
+                res[away_ream] = res[away_ream] + 3
+            else:
+                res[away_ream] = 3
+
+        i += 1
+
+    return max(res, key=res.get)
 
 
-print(tournamentWinner({
-    "competitions": [
+print(tournamentWinner(
+    [
         ["HTML", "C#"],
         ["C#", "Python"],
         ["Python", "HTML"]
     ],
-    "results": [0, 0, 1]
-}))
+    [0, 0, 1]
+))
